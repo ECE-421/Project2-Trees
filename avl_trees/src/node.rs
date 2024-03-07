dc5bbb460c1eb9932e0c164a3ce548e4625df6ed
iDeniyi <inioluwa@ualberta.cai>
2024-03-07 16:54:15 -0700

    modified the node struct

--- avl_trees/src/node.rs
+++ avl_trees/src/node.rs
@@ -1,62 +1,60 @@
 use std::{cell::RefCell, cmp, rc::Rc};
 
 type NodePtr<T> = Option<Rc<RefCell<Node<T>>>>;
-
-struct Node<T: Ord> {
-    value: T,
+struct Node<T> {
+    data: T,
     height: i32,
     left: NodePtr<T>,
-    right: NodePtr<T>
+    right: NodePtr<T>,
 }
 
 impl<T: Ord> Node<T> {
-    fn new(value: T) -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Node{
-            value,
-            height: 1,
-            left: None,
-            right: None
-        }))
+    fn new(data: T) -> Self {
+        Self {data, height: 1, left: None, right: None}
+    }
+
+    fn subtree_height(node: &NodePtr<T>) -> i32 {
+        node.as_ref().map_or(0, |node| node.borrow().height)
     }
 
     fn update_height(&mut self) {
-        let left_height = self.left.as_ref().map_or(0, |left| left.borrow().height);
-        let right_height = self.right.as_ref().map_or(0, |right| right.borrow().height);
-        self.height = 1 + std::cmp::max(left_height, right_height);
+        let left_height = Self::subtree_height(&self.left);
+        let right_height = Self::subtree_height(&self.right);
+        self.height = 1 + cmp::max(left_height, right_height);
     }
 
     fn balance_factor(&self) -> i32 {
-        let left_height = self.left.as_ref().map_or(0, |left| left.borrow().height);
-        let right_height = self.right.as_ref().map_or(0, |right| right.borrow().height);
+        let left_height = Self::subtree_height(&self.left);
+        let right_height = Self::subtree_height(&self.right);
         left_height - right_height
     }
-    
 
-    pub fn get_height(&self) -> i32 {
+    pub fn height(&self) -> i32 {
         self.height
     }
 
 
-    fn rotate_right(node: Rc<RefCell<Self>>) -> NodePtr<T> {
-        let node_left = node.borrow_mut().left.take().expect("Left child must exist for right rotation");
-        node.borrow_mut().left = node_left.borrow_mut().right.take();
-        node_left.borrow_mut().right = Some(node.clone());
+    fn rotate_right(root: Rc<RefCell<Self>>) -> NodePtr<T> {
+        let new_root = root.borrow_mut().left.take().expect("Left child must exist for right rotation");
+        root.borrow_mut().left = new_root.borrow_mut().right.take();
+        new_root.borrow_mut().right = Some(root.clone());
 
-        node.borrow_mut().update_height();
-        node_left.borrow_mut().update_height();
+        root.borrow_mut().update_height();
+        new_root.borrow_mut().update_height();
 
-        Some(node_left)
+        Some(new_root)
     }
+    
 
-    fn rotate_left(node: Rc<RefCell<Self>>) -> NodePtr<T> {
-        let node_right = node.borrow_mut().right.take().expect("Right child must exist for left rotation");
-        node.borrow_mut().right = node_right.borrow_mut().left.take();
-        node_right.borrow_mut().left = Some(node.clone());
+    fn rotate_left(root: Rc<RefCell<Self>>) -> NodePtr<T> {
+        let new_root = root.borrow_mut().right.take().expect("Right child must exist for left rotation");
+        root.borrow_mut().right = new_root.borrow_mut().left.take();
+        new_root.borrow_mut().left = Some(root.clone());
 
-        node.borrow_mut().update_height();
-        node_right.borrow_mut().update_height();
+        root.borrow_mut().update_height();
+        new_root.borrow_mut().update_height();
 
-        Some(node_right)
+        Some(new_root)
     }
 
 }
@@ -69,64 +67,60 @@ mod tests {
     #[test]
     fn test_new_node() {
         let node = Node::new(10);
-        assert_eq!(node.borrow().value, 10);
-        assert_eq!(node.borrow().height, 1);
-        assert!(node.borrow().left.is_none());
-        assert!(node.borrow().right.is_none());
+        assert_eq!(node.data, 10); 
+        assert_eq!(node.height, 1); 
+        assert!(node.left.is_none());
+        assert!(node.right.is_none());
     }
 
     #[test]
     fn test_update_height() {
-        let node = Node::new(10);
-        let left_child = Node::new(5);
-        let right_child = Node::new(15);
+        let mut node = Node::new(10);
+        let left_child = Rc::new(RefCell::new(Node::new(5)));
+        let right_child = Rc::new(RefCell::new(Node::new(15)));
 
-        node.borrow_mut().left = Some(left_child.clone());
-        node.borrow_mut().right = Some(right_child.clone());
+        node.left = Some(left_child.clone());
+        node.right = Some(right_child.clone());
 
-        // Manually set the height of children to simulate a tree structure
         left_child.borrow_mut().height = 2;
         right_child.borrow_mut().height = 3;
 
-        node.borrow_mut().update_height();
+        node.update_height();
 
-        assert_eq!(node.borrow().height, 4);
+        assert_eq!(node.height, 4);
     }
 
     #[test]
     fn test_balance_factor() {
-        let node = Node::new(10);
-        let left_child = Node::new(5);
-        let right_child = Node::new(15);
+        let mut node = Node::new(10);
+        let left_child = Rc::new(RefCell::new(Node::new(5)));
+        let right_child = Rc::new(RefCell::new(Node::new(15)));
 
-        node.borrow_mut().left = Some(left_child.clone());
-        node.borrow_mut().right = Some(right_child.clone());
+        node.left = Some(left_child.clone());
+        node.right = Some(right_child.clone());
 
-        // Manually set the height of children to simulate a tree structure
         left_child.borrow_mut().height = 2;
         right_child.borrow_mut().height = 3;
 
-        // Update parent node height after changing children
-        node.borrow_mut().update_height();
+        node.update_height();
 
-        let balance_factor = node.borrow().balance_factor();
-        assert_eq!(balance_factor, -1); // Right subtree is taller by 1
+        let balance_factor = node.balance_factor();
+        assert_eq!(balance_factor, -1);
     }
 
     #[test]
     fn test_rotate_right() {
-        let root = Node::new(10);
-        let left_child = Node::new(5);
-        let left_left_child = Node::new(2);
+        let root = Rc::new(RefCell::new(Node::new(10)));
+        let left_child = Rc::new(RefCell::new(Node::new(5)));
+        let left_left_child = Rc::new(RefCell::new(Node::new(2)));
 
         root.borrow_mut().left = Some(left_child.clone());
         left_child.borrow_mut().left = Some(left_left_child.clone());
 
         let new_root = Node::rotate_right(root.clone()).unwrap();
 
-        assert_eq!(new_root.borrow().value, 5);
+        assert_eq!(new_root.borrow().data, 5);
         assert!(new_root.borrow().right.is_some());
-        assert_eq!(new_root.borrow().right.as_ref().unwrap().borrow().value, 10); 
+        assert_eq!(new_root.borrow().right.as_ref().unwrap().borrow().data, 10);
     }
 }
-
