9b8bad027637363bd64df20aa65e8b6a09d0d12a
iDeniyi <inioluwa@ualberta.cai>
2024-03-19 12:44:18 -0600

    implemented a simple benchmark for the insert and delete functions. the delete function will need to be changed to search to follow the assignment requirement

---
+++ avl_trees/benches/benchmark.rs
@@ -0,0 +1,30 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use avl_trees::tree::Tree;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    for &size in &[10000, 40000, 70000, 100000, 130000] {
+        let mut tree: Tree<i64> = Tree::new();
+        let values: Vec<i64> = (0..size).collect();
+        c.bench_function(&format!("insert_{}", size), |b| {
+            b.iter(|| {
+                for &value in &values {
+                    tree.insert(black_box(value));
+                }
+            });
+        });
+
+        for &value in &values {
+            tree.insert(black_box(value));
+        }
+        c.bench_function(&format!("search_{}", size), |b| {
+            b.iter(|| {
+                for &value in &values {
+                    tree.delete(black_box(value));
+                }
+            });
+        });
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
